b26d99208efc042c5b4cfb287a56177e8a0401d9
MicroSugar <wufeng4728704@hotmail.com>
2022-12-30 17:50:25 +0800

    styles

--- Source/AntDesign/Helpers/PARTNameHelper.cs
+++ Source/AntDesign/Helpers/PARTNameHelper.cs
@@ -1,7 +1,22 @@
 ﻿namespace AntDesign.Helpers;
 public static class PARTNameHelper
 {
-    public static string PARTTransparencyFallback => "PART_TransparencyFallback";
-    public static string PARTContentPresenter => "PART_ContentPresenter";
+    public static string PART_TransparencyFallback => "PART_TransparencyFallback";
+    public static string PART_ContentPresenter => "PART_ContentPresenter";
 
+    public static string PART_MouseTracker => "PART_MouseTracker";
+    public static string PART_Container => "PART_Container";
+    public static string PART_Background => "PART_Background";
+    public static string PART_CaptionButtons => "PART_CaptionButtons";
+
+    public static string PART_FullScreenButton => "PART_FullScreenButton";
+    public static string PART_MinimiseButton => "PART_MinimiseButton";
+    public static string PART_RestoreButton => "PART_RestoreButton";
+    public static string PART_CloseButton => "PART_CloseButton";
+
+    public static string PART_LayoutRoot => "PART_LayoutRoot";
+    public static string PART_TextBox => "PART_TextBox";
+    public static string PART_Popup => "PART_Popup";
+    public static string PART_SuggestionsContainer => "PART_SuggestionsContainer";
+    public static string PART_SelectingItemsControl => "PART_SelectingItemsControl";
 }
