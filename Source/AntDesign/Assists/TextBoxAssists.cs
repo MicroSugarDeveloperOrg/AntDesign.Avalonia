5a18291b38541bb9a59d92256355cd1f729b4d79
MicroSugar <wufeng4728704@hotmail.com>
2023-01-04 16:52:41 +0800

    add floatingForeground for TextBox

--- Source/AntDesign/Assists/TextBoxAssists.cs
+++ Source/AntDesign/Assists/TextBoxAssists.cs
@@ -12,6 +12,10 @@ public static class TextBoxAssists
     public static void SetFocusBorderBrush(AvaloniaObject dependencyObject, IBrush value) => dependencyObject.SetValue(FocusBorderBrushProperty, value);
     public static IBrush? GetFocusBorderBrush(AvaloniaObject dependencyObject) => dependencyObject.GetValue<IBrush?>(FocusBorderBrushProperty);
 
+    public static readonly AvaloniaProperty<IBrush?> FloatingForegroundProperty = AvaloniaProperty.RegisterAttached<TextBox, IBrush?>("FloatingForeground", typeof(TextBoxAssists));
+    public static void SetFloatingForeground(AvaloniaObject dependencyObject, IBrush value) => dependencyObject.SetValue(FloatingForegroundProperty, value);
+    public static IBrush? GetFloatingForeground(AvaloniaObject dependencyObject) => dependencyObject.GetValue<IBrush?>(FloatingForegroundProperty);
+
     public static readonly AvaloniaProperty<Color> RippleColorProperty = AvaloniaProperty.RegisterAttached<TextBox, Color>("RippleColor", typeof(TextBoxAssists));
     public static void SetRippleColor(AvaloniaObject dependencyObject, Color value) => dependencyObject.SetValue(RippleColorProperty, value);
     public static Color GetRippleColor(AvaloniaObject dependencyObject) => dependencyObject.GetValue<Color>(RippleColorProperty);
