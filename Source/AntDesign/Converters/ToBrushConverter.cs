1ddddf48c3c5928de79f44961748f960310f67d3
MicroSugar <wufeng4728704@hotmail.com>
2023-01-06 10:42:48 +0800

    arrangement

--- Source/AntDesign/Converters/ToBrushConverter.cs
+++ Source/AntDesign/Converters/ToBrushConverter.cs
@@ -12,21 +12,14 @@ public class ToBrushConverter : IValueConverter
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         if (value is IBrush brush)
-        {
             return brush;
-        }
         else if (value is Color valueColor)
-        {
             return new SolidColorBrush(valueColor);
-        }
+
         else if (value is HslColor valueHslColor)
-        {
             return new SolidColorBrush(valueHslColor.ToRgb());
-        }
         else if (value is HsvColor valueHsvColor)
-        {
             return new SolidColorBrush(valueHsvColor.ToRgb());
-        }
 
         return AvaloniaProperty.UnsetValue;
     }
