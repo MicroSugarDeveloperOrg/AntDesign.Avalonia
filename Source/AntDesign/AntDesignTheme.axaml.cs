9d13ead92c437e727c6699b18f86602cfe7364ad
MicroSugar <wufeng4728704@hotmail.com>
2023-02-04 23:20:24 +0800

    update to  preview5

---
+++ Source/AntDesign/AntDesignTheme.axaml.cs
@@ -0,0 +1,12 @@
+﻿using AntDesign.Extensions;
+using Avalonia.Markup.Xaml.Styling;
+
+namespace AntDesign;
+
+public class AntDesignTheme : Styles
+{
+    public AntDesignTheme(IServiceProvider serviceProvider)
+    {
+        AvaloniaXamlLoader.Load(serviceProvider, this);
+    }
+}
