055280391340b6234f04552bba2eb58089d33578
吴锋 <wufeng4728704@hotmainl.com>
2023-02-17 21:20:35 +0800

    change

--- Source/Avalonia.ReactiveUI.Toolkit/ReactiveObjects/ReactiveObject@.cs
+++ Source/Avalonia.ReactiveUI.Toolkit/ReactiveObjects/ReactiveObject@.cs
@@ -1,39 +1,6 @@
 ﻿namespace Avalonia.ReactiveUI.Toolkit.ReactiveObjects;
 
-public class ReactiveObject<T> : ReactiveObject
+public class ReactiveObject<T> : ReactiveObjectBase
 {
     public T? TObject { get; protected set; }
-
-    public TRet SetProperty<TRet>(ref TRet backingField, TRet newValue, [CallerMemberName] string? propertyName = null) => this.RaiseAndSetIfChanged(ref backingField, newValue, propertyName);
-
-    public TRet SetProperty<TRet>(ref TRet backingField, TRet newValue, Action<TRet> propertyChanging, [CallerMemberName] string? propertyName = null) =>
-        SetProperty(ref backingField, newValue, propertyChanging, default, propertyName);
-
-    public TRet SetProperty<TRet>(ref TRet backingField, TRet newValue, Action<TRet, TRet> propertyChanged, [CallerMemberName] string? propertyName = null) =>
-        SetProperty(ref backingField, newValue, default, propertyChanged, propertyName);
-
-    public TRet SetProperty<TRet>(ref TRet backingField, TRet newValue, Action<TRet>? propertyChanging = default, Action<TRet, TRet>? propertyChanged = default, [CallerMemberName] string? propertyName = null)
-    {
-        var oldValue = backingField;
-        void Invoking(object? sender, PropertyChangingEventArgs e)
-        {
-            if (e.PropertyName != propertyName)
-                return;
-            propertyChanging?.Invoke(oldValue);
-        }
-
-        void Invoked(object? sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName != propertyName)
-                return;
-            propertyChanged?.Invoke(oldValue, newValue);
-        }
-
-        this.PropertyChanging += Invoking;
-        this.PropertyChanged += Invoked;
-        var ret = this.RaiseAndSetIfChanged(ref backingField, newValue, propertyName);
-        this.PropertyChanging -= Invoking;
-        this.PropertyChanged -= Invoked;
-        return ret;
-    }
 }
