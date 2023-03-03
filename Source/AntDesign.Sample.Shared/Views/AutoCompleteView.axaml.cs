91f8e6f41e5b8b436ff52bf668286408929509ea
MicroSugar <wufeng4728704@hotmail.com>
2023-03-03 21:07:33 +0800

    add new design

--- Source/AntDesign.Sample.Shared/Views/AutoCompleteView.axaml.cs
+++ Source/AntDesign.Sample.Shared/Views/AutoCompleteView.axaml.cs
@@ -1,11 +1,11 @@
 using AntDesign.Sample.ViewModels;
+using Avalonia.Media;
 
 namespace AntDesign.Sample.Views;
 public partial class AutoCompleteView : ReactiveUserControl<AutoCompleteViewModel>
 {
     public AutoCompleteView()
     {
-        InitializeComponent(); 
-       
+        InitializeComponent();  
     }
 }
