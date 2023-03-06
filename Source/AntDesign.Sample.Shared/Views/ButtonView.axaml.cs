02fa1e28354387027eda306e490dc30da8555db8
MicroSugar <wufeng4728704@hotmail.com>
2023-03-06 13:55:35 +0800

    add new design for repeatbutton

--- Source/AntDesign.Sample.Shared/Views/ButtonView.axaml.cs
+++ Source/AntDesign.Sample.Shared/Views/ButtonView.axaml.cs
@@ -1,6 +1,4 @@
 using AntDesign.Sample.ViewModels;
-using Avalonia.Controls.Presenters;
-using Avalonia.Media;
 
 namespace AntDesign.Sample.Views;
 public partial class ButtonView : ReactiveUserControl<ButtonViewModel>
@@ -8,54 +6,6 @@ public partial class ButtonView : ReactiveUserControl<ButtonViewModel>
     public ButtonView()
     {
         InitializeComponent();
-        //MenuItem.IsSubMenuOpenProperty
-        //Button.Cursor
-        //Popup.IsOpen
-        //ComboBox.focus - visible
-
-        //MenuItem.ItemsProperty
-
-        //ContentTemplate
-
-        //ToggleSwitch
-
-        //MultiBinding
-        //Bounds
-
-        //RenderTransformOrigin
-
-        //MarginMultiplierConverter
-        //ColumnDefinition
-
-        //CalendarButton
-        //CalendarDatePicker.DisplayDateProperty
-        //TextPresenter
-
-        //RowDefinition
-
-        //TreeView
-
-        //int.TryParse(,System.Globalization.NumberStyles.HexNumber)
-        //TryException();
-        //RotateTransform
-        //TreeView.ItemsPanelProperty
-        //TimePicker
-        //TimePickerPresenter.
-        //Avalonia.Controls.Shapes.Path.VerticalAlignmentProperty
-
-        //Border.BoxShadowProperty
-
-        //DatePicker.
-        //Classes.error
-
-        //Button
-        //BorderThicknessFilterConverter
-        //CornerRadiusFilterConverter
-        //DoubleTransition
-        //ColorTransition
-        //CornerRadiusTransition
-        //Colors.DarkGreen 
-        
     }
 
 }
