d0c392ded03b4b624d5d3ae586f4b342ebe673d3
MicroSugar <wufeng4728704@hotmail.com>
2023-03-16 09:11:52 +0800

    add numberupdown view

--- Source/AntDesign.Sample.Shared/ViewModels/NumericUpDownViewModel.cs
+++ Source/AntDesign.Sample.Shared/ViewModels/NumericUpDownViewModel.cs
@@ -3,4 +3,10 @@
 namespace AntDesign.Sample.ViewModels;
 public class NumericUpDownViewModel : ViewModelRoutableBase<NumericUpDownViewModel>
 {
+    private string _formater = "{0}";
+    public string Formater
+    {
+        get => _formater;
+        set => SetProperty(ref _formater, value);
+    }
 }
