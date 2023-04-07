cbde16cc7b738d7190c9a3d8c7e2685763d0770c
MicroSugar <wufeng4728704@hotmail.com>
2023-04-07 13:06:51 +0800

    add newdesign for datagrid

---
+++ Source/AntDesign.DataGrid/Converters/GridLine2HeightConverter.cs
@@ -0,0 +1,27 @@
+﻿namespace AntDesign.Converters;
+public class GridLine2HeightConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (Enum.TryParse<DataGridGridLinesVisibility>(value?.ToString(), out var gridLine))
+        {
+            switch (gridLine)
+            {
+                case DataGridGridLinesVisibility.Vertical:
+                case DataGridGridLinesVisibility.All:
+                    return double.NaN;
+                case DataGridGridLinesVisibility.None:
+                case DataGridGridLinesVisibility.Horizontal: 
+                default:
+                    return 20d;
+            }
+        }
+
+        return double.NaN;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
