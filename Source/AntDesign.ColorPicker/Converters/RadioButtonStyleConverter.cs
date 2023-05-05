608485ef5849327238a82e4a1db4d77dd5b9232a
MicroSugar <wufeng4728704@hotmail.com>
2023-05-05 10:11:46 +0800

    add new design

---
+++ Source/AntDesign.ColorPicker/Converters/RadioButtonStyleConverter.cs
@@ -0,0 +1,42 @@
+﻿using Avalonia.Controls;
+using Avalonia.Data.Converters;
+using Avalonia.Markup.Xaml.XamlIl.Runtime;
+using System.Globalization;
+
+namespace AntDesign.Converters;
+internal class RadioButtonStyleConverter : IValueConverter
+{
+    object? IValueConverter.Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value == null && parameter is not null)
+            return parameter;
+
+        return value;
+    }
+
+    object? IValueConverter.ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+
+    internal static ThemeVariant? GetDictionaryVariant(IAvaloniaXamlIlParentStackProvider service)
+    {
+        IEnumerable<object>? enumerable = service?.Parents;
+        if (enumerable == null)
+            return null;
+
+        foreach (var item in enumerable)
+        {
+            var themeVariantProvider = item as IThemeVariantProvider;
+            if (themeVariantProvider != null)
+            {
+                var key = themeVariantProvider.Key;
+                if (key != null)
+                    return key;
+            }
+        }
+
+        return null;
+    }
+
+}
---
+++ Source/Avalonia.Toolkit/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using Avalonia.Metadata;
+
+[assembly: XmlnsPrefix("https://github.com/avaloniaui", $"{nameof(Avalonia)}.{nameof(Avalonia.Toolkit)}")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", $"{nameof(Avalonia)}.{nameof(Avalonia.Toolkit)}")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", $"{nameof(Avalonia)}.{nameof(Avalonia.Toolkit)}.{nameof(Avalonia.Toolkit.Media)}")]
+[assembly: XmlnsDefinition("https://github.com/avaloniaui", $"{nameof(Avalonia)}.{nameof(Avalonia.Toolkit)}.{nameof(Avalonia.Toolkit.Settings)}")]
+ 
+
+[assembly: XmlnsPrefix("https://ant.design.com/avalonia", $"{nameof(Avalonia)}.{nameof(Avalonia.Toolkit)}")]
+[assembly: XmlnsDefinition("https://ant.design.com/avalonia", $"{nameof(Avalonia)}.{nameof(Avalonia.Toolkit)}")]
+[assembly: XmlnsDefinition("https://ant.design.com/avalonia", $"{nameof(Avalonia)}.{nameof(Avalonia.Toolkit)}.{nameof(Avalonia.Toolkit.Media)}")]
+[assembly: XmlnsDefinition("https://ant.design.com/avalonia", $"{nameof(Avalonia)}.{nameof(Avalonia.Toolkit)}.{nameof(Avalonia.Toolkit.Settings)}")]
